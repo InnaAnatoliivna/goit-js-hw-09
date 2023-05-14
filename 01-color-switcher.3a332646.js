13242767c084f4321c2a9793d15a3a88d2033689
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-05-14 15:51:25 +0000

    Deploying to gh-pages from @ InnaAnatoliivna/goit-js-hw-09@62b0420eb90055e16a8e4a466ef2fd745996d407 🚀

--- 01-color-switcher.3a332646.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d;function a(){d=setInterval(a,1e3);const n=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=n,t.disabled=!0,e.disabled=!1}e.disabled=!0,t.addEventListener("click",a),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.3a332646.js.map
