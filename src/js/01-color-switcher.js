05f8bafc7fd5411531fc1afc30f0d0bf88de1791
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-05-14 17:17:51 +0200

    first commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,50 @@
-console.log('hello')
+// Напиши скрипт, який після натискання кнопки «Start», раз на 
+// секунду змінює колір фону < body > на випадкове значення,
+// використовуючи інлайн стиль.Натисканням на кнопку «Stop» 
+//     зміна кольору фону повинна зупинятися.
+
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+let intervalChangeBackgroundColor;
+
+buttonStop.disabled = true;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+buttonStart.addEventListener('click', onStartChangeBackgroundColor);
+buttonStop.addEventListener('click', onStopChangeBackgroundColor);
+
+
+//function for used in handlers
+function startIntervalChanges() {
+    intervalChangeBackgroundColor = setInterval(onStartChangeBackgroundColor, 1000)
+};
+function stopIntervalChanges() {
+    clearInterval(intervalChangeBackgroundColor);
+};
+
+
+//function - handler
+function onStartChangeBackgroundColor() {
+
+    startIntervalChanges();
+
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+};
+
+function onStopChangeBackgroundColor() {
+    stopIntervalChanges();
+
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+};
+
+
+
